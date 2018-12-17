f049002e64a2cd4063dec2326c5c45d26e563ff2
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-12-17 13:54:21 -0800

    Create AppRouter with basic navigation for needed pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import AppRouter from './AppRouter'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+
+      <div>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <AppRouter />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
