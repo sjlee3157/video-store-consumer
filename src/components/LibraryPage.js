c9e67610912c539008bfa39be289193c21430096
sjlee3157 <sjlee3157@gmail.com>
2018-12-17 16:19:59 -0800

    Fix LibraryPage component

--- src/components/LibraryPage.js
+++ src/components/LibraryPage.js
@@ -1,86 +1,17 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Movie from "./Movie"
+import React from 'react';
+
 import PropTypes from 'prop-types';
 import './styles/LibraryPage.css';
 
 import LibraryList from './LibraryList.js';
 
-const URL = 'http://localhost:3000/movies/'
-
-class LibraryPage extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      movies: []
-      }
-  }
-
-
-  componentDidMount() {
-    axios.get(URL)
-      .then((response) => {
-        const movieSet = response.data.map((movie) => {
-          console.log(movie)
-          const newMovie = {
-            ...movie,
-          };
-          console.log(newMovie)
-          return newMovie;
-        })
-
-        this.setState({
-          movies: movieSet
-        });
-
-      })
-      .catch((error) => {
-        console.log(error.message);
-        this.setState({
-          errorMessage: error.message,
-        });
-      });
-  }
-
-
-  render() {
-<<<<<<< HEAD
-    console.log("Rendering!")
+const LibraryPage = (props) => {
     return (
       <div>
         <h1> Library Page :)</h1>
         <LibraryList />
       </div>
     )
-
-=======
-    console.log("Rendered Movies:", this.state.movies)
-    const allMovies = this.state.movies
-      .map((movie, i) => {
-        return (
-          <Movie
-            key={i}
-            externalId={movie.external_id}
-            id={movie.id}
-            imageUrl ={movie.image_url}
-            overview={movie.overview}
-            releaseDate={movie.release_date}
-            title={movie.title}
-          />
-        )
-      })
-    return (
-      <div>
-        <ul>
-         {allMovies}
-        </ul>
-       </div>
-    )
-
->>>>>>> abf4cd546e6fe3667b81052970768ff8f6399dfe
-  }
-
 }
 
 export default LibraryPage;
