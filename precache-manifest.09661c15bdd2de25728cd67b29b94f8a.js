6794604a66a65e6459608e646f28c60724fa442a
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-12-19 14:48:01 -0800

    Updates

--- precache-manifest.09661c15bdd2de25728cd67b29b94f8a.js
+++ precache-manifest.09661c15bdd2de25728cd67b29b94f8a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "0c60e51f404a05dc7862",
+    "revision": "f126dcc0b94b1b61497b",
     "url": "/video-store-consumer/static/css/main.3dc43f5b.chunk.css"
   },
   {
-    "revision": "0c60e51f404a05dc7862",
-    "url": "/video-store-consumer/static/js/main.0c60e51f.chunk.js"
+    "revision": "f126dcc0b94b1b61497b",
+    "url": "/video-store-consumer/static/js/main.f126dcc0.chunk.js"
   },
   {
     "revision": "2958064aa1c39ab33528",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/video-store-consumer/static/js/runtime~main.bfa7f193.js"
   },
   {
-    "revision": "27b444fa98a67fcab8454ad4a18ddb86",
+    "revision": "ca48e00068b7a7095784a5bcb25fc42c",
     "url": "/video-store-consumer/index.html"
   }
 ];
